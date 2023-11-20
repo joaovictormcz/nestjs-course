6a251d18827588fc1d9c6d951af705493cfabceb
joaovictormcz <joaovictor.mcz@gmail.com>
2023-11-20 01:29:00 +0000

    supabase

---
+++ src/infra/providers/storage/supabase.storage.ts
@@ -0,0 +1,24 @@
+import { FileDTO } from "src/modules/users/dto/user.dto";
+import { IStorage } from "./storage";
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class SupabaseStoareg implements IStorage {
+  private client: SupabaseClient;
+  constructor() {
+    this.client = createClient(
+      process.env.SUPABASE_URL ?? "",
+      process.env.SUPABASE_KEY ?? ""
+    );
+  }
+
+  async upload(file: FileDTO, folder: string): Promise<any> {
+    const data = await this.client.storage
+      .from(process.env.SUPABASE_BUCKET ?? '')
+      .upload(`${folder}/` + file.originalname, file.buffer, {
+        upsert: true,
+      });
+  }
+}
+ 
